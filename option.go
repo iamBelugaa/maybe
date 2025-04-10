97bbd419e48779cb579527817fe2f59ce64f6c36
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2025-04-11 01:03:12 +0530

    - add FromPtr() to convert a pointer to an Option. - add Set() to update the Option to contain the provided value. - add Unset() to clear the Option, changing it to None.

--- option.go
+++ option.go
@@ -20,3 +20,31 @@ func None[T any]() Option[T] {
 	var value T
 	return Option[T]{has: false, value: value}
 }
+
+// FromPtr converts a pointer to an Option.
+// Returns Some(value) if the pointer is non-nil, or None if the pointer is nil.
+func FromPtr[T any](ptr *T) Option[T] {
+	if IsNil(ptr) {
+		return None[T]()
+	}
+	return Some(*ptr)
+}
+
+// Set updates the Option to contain the provided value.
+// Changes None to Some(value) or updates an existing Some value.
+func (o *Option[T]) Set(v T) {
+	if !o.has {
+		o.has = true
+	}
+	o.value = v
+}
+
+// Unset clears the Option, changing it to None.
+// The contained value (if any) is set to the zero value of type T.
+func (o *Option[T]) Unset() {
+	if o.has {
+		o.has = false
+	}
+	var zero T
+	o.value = zero
+}
